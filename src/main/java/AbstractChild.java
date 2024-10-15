b06ae2423eee5f2be1daf07671766a1b9dee7836
Alper <alperdevir@gmail.com>
2024-10-15 21:36:06 +0300

    d23

---
+++ src/main/java/AbstractChild.java
@@ -0,0 +1,7 @@
+import d23oop_collections.Honda;
+
+public abstract class AbstractChild extends  Honda{
+    //Abstract child, abstract parentinin methodlarini implement etmek
+    //zorunde degildir.
+
+}
---
+++ src/main/java/d23oop_collections/HondaRunner.java
@@ -0,0 +1,12 @@
+package d23oop_collections;
+
+public class HondaRunner {
+    public static void main(String[] args) {
+
+
+        Civic c =new  Civic();
+        c.engine();
+
+
+    }
+}
